e658c52dd9c39da00ddcb890fecca0af2708d1f3
ChSPN <charlene.scomparin@gmail.com>
2024-03-27 21:52:36 +0100

    Fix the gcc compilation

--- _printf.c
+++ _printf.c
@@ -10,59 +10,49 @@ int _printf(const char *format, ...)
 
 	va_start(args, format);
 
+	/* Lecture de la chaine de caracteres */
 	while (*format != '\0')
 	{
-		/* Detection du caractere pourcent */
-		if (*format != '%')
+		if (flag > 1)
 		{
-			/* Detection du formatage */
-			if (flag > 0)
+			/* Remise à zero de la dectection */
+			flag = 0;
+
+			/* Recuperation de la fonction de formatage */
+			ptr_func = get_print_func(format);
+
+			/* Detection du formatage non pris en charge */
+			if (ptr_func != NULL)
 			{
-				/* Remise à zero de la dectection */
-				flag = 0;
-
-				/* Recuperation de la fonction de formatage */
-				ptr_func = get_print_func(format);
-
-				/* Detection du formatage non pris en charge */
-				if (ptr_func != NULL)
-				{
-					/* Execution du formatage */
-					length += ptr_func(args);
-					
-					/* Incrementation de l'adresse du pointeur de caractere */
-					format++;
-					continue;
-				}
-				else
-				{
-					/* Affichage du pourcentage */
-					length += print_percent();
-				}
+				/* Execution du formatage */
+				length += ptr_func(args);
 			}
-			
+			else
+			{
+				/* Affichage du format non reconnu */
+				_putchar('%');
+				_putchar(*format);
+				length += 2;
+			}
+		}
+		else if (*format != '%')
+		{
 			/* Affichage normal du caractere */
 			_putchar(*format);
 
 			/* Incrementation du nombre de caractere */
 			length++;
 		}
-		else if (flag == 0)
+		else
 		{
 			/* Detection d'un formatage */
 			flag = 1;
 		}
-		else
-		{
-			/* Detection d'un double pourcent */
-			length += print_percent();
-		}
-
+		
 		/* Incrementation de l'adresse du pointeur de caractere */
 		format++;
 	}
 
-
-	va_end(arg);
+	va_end(args);
 	return (length);
 }
