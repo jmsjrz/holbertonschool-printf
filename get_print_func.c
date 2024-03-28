2d294316a288be117c216e373f6b5a65311482e9
James <8708@holbertonstudents.com>
2024-03-28 14:48:25 +0100

    update get_print_function

--- get_print_func.c
+++ get_print_func.c
@@ -1,25 +1,32 @@
 #include "main.h"
 
 /**
- * get_print_func - function who print
- * Description: get_print_func
- * @identifier: format of the value
- * Return: pointer of print function
+* get_print_func - Retrieves a function pointer for handling a specific format.
+* @identifier: A string representing the format identifier.
+*
+* Description: Iterates over an array of format_t structs to find a matching
+* format handler for the given identifier. The function compares only the
+* first character of the identifier to find a match.
+*
+* Return: Pointer to the matched print function, or NULL if no match is found.
 */
 int (*get_print_func(const char *identifier))(va_list)
 {
-	int i;
+	int index;
+
 	format_t prints[] = {
 		{"c", print_char},
 		{"s", print_string},
 		{"%", print_percent},
+		{"d", print_integer},
+		{"i", print_integer},
 	};
 
-	for (i = 0 ; i < 2 ; i++)
+	for (index = 0; index < 5; index++)
 	{
-		if (prints[i].identifier == identifier)
+		if (prints[index].identifier[0] == identifier[0])
 		{
-			return (prints[i].ptr_func);
+			return (prints[index].ptr_func);
 		}
 	}
 
