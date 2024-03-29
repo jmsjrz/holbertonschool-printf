7f2ec93894579a4902a076cb452fb6ddafba19c8
James <8708@holbertonstudents.com>
2024-03-29 10:51:58 +0100

    update _printf function

--- _printf.c
+++ _printf.c
@@ -1,58 +1,59 @@
 #include "main.h"
 
+/**
+* _printf - Custom implementation of the printf function.
+* @format: The format string that specifies the format to output.
+* @...: A variable number of arguments to be formatted and printed.
+*
+* Description: This function mimics the standard 'printf' behavior, printing
+* the formatted output to the standard output (stdout). It supports format
+* specifiers for printing characters (%c), strings (%s), percentages (%%),
+* and integers (%d, %i). The function iterates over the format string and
+* processes each character and format specifier accordingly. When a format
+* specifier is encountered, it delegates the processing to the corresponding
+* function pointed to within an array of structs.
+*
+* Return: The total number of characters printed, excluding the null byte.
+*/
 int _printf(const char *format, ...)
 {
-	/* declaration des variables de fonctionnement */
-	int length = 0;
-	int flag = 0;
 	va_list args;
-	int (*ptr_func)(va_list args);
+	int count = 0; /* Counter for the number of characters printed */
+	int index; /* Index for iterating through the format specifier array */
 
-	va_start(args, format);
+	/* Array mapping format specifiers to their respective functions */
+	format_t prints[] = {
+		{"c", print_char},
+		{"s", print_string},
+		{"%", print_percent},
+		{"d", print_integer},
+		{"i", print_integer},
+		{NULL, NULL}
+	};
 
-	/* Lecture de la chaine de caracteres */
-	while (*format != '\0')
+	va_start(args, format); /* Initializing the argument list */
+	while (*format)
 	{
-		if (flag > 1)
+		if (*format == '%') /* Check for format specifier */
 		{
-			/* Remise à zero de la dectection */
-			flag = 0;
-
-			/* Recuperation de la fonction de formatage */
-			ptr_func = get_print_func(format);
-
-			/* Detection du formatage non pris en charge */
-			if (ptr_func != NULL)
+			format++; /* Move to the format specifier character */
+			index = 0; /* Reset index for each format specifier */
+			while (prints[index].identifier)
 			{
-				/* Execution du formatage */
-				length += ptr_func(args);
+				if (*format == *(prints[index].identifier))
+				{
+					count += prints[index].ptr_func(args); /* Call handler */
+					break; /* Exit the loop once the handler is called */
+				}
+				index++;
 			}
-			else
-			{
-				/* Affichage du format non reconnu */
-				_putchar('%');
-				_putchar(*format);
-				length += 2;
-			}
-		}
-		else if (*format != '%')
-		{
-			/* Affichage normal du caractere */
-			_putchar(*format);
-
-			/* Incrementation du nombre de caractere */
-			length++;
 		}
 		else
 		{
-			/* Detection d'un formatage */
-			flag = 1;
+			count += _putchar(*format); /* Print non-format characters */
 		}
-		
-		/* Incrementation de l'adresse du pointeur de caractere */
-		format++;
+		format++; /* Move to the next character in the format string */
 	}
-
-	va_end(args);
-	return (length);
+	va_end(args); /* Clean up the argument list */
+	return (count); /* Return the count of printed characters */
 }
