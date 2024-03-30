133994d3ef71ea30e9a41d8a81deec9b44b2e36e
James <8708@holbertonstudents.com>
2024-03-30 15:02:33 +0100

    fix null string

--- print_string.c
+++ print_string.c
@@ -6,8 +6,10 @@
 *
 * Description: Extracts and outputs a string from the variable argument list,
 * character by character, until the null terminator is encountered. If the
-* provided string is NULL, the function will simply return without printing
-* anything. This function is intended to serve as a handler for the '%s'
+* provided string is NULL, the function will print "(null)" instead. This
+* approach avoids recursive calls to _printf, ensuring accurate character
+* counting and adherence to expected behavior for printing NULL strings.
+* This function is intended to serve as a handler for the '%s'
 * format specifier in a printf-like function.
 *
 * Return: The number of characters printed, excluding the null terminator.
@@ -16,23 +18,28 @@ int print_string(va_list args)
 {
 	int count = 0; /* Character count */
 
-	/* Retrieve string from va_list */
 	char *string = va_arg(args, char*);
 
-	/* Handle NULL string case */
+	char *null_string = "(null)";
+
 	if (string != NULL)
 	{
 		/* Iterate over each character in the string */
 		while (*string != '\0')
 		{
 			_putchar(*string); /* Output current character */
-			count++;     /* Increment character count */
+			count++;           /* Increment character count */
 			string++;          /* Advance to next character */
 		}
 	}
 	else
 	{
-		count = _printf("(null)");
+		/* Directly print "(null)" for NULL strings */
+		while (*null_string != '\0')
+		{
+			_putchar(*null_string++); /* Output current character */
+			count++;                  /* Increment character count */
+		}
 	}
 
 	return (count); /* Return total character count */
